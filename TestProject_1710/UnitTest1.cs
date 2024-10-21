2c86f219c49592289546f258e470f0eb0e016717
Thỏa Kim <thoadinh000@gmail.com>
2024-10-21 18:39:21 +0700

    'test'

---
+++ TestProject_1710/UnitTest1.cs
@@ -0,0 +1,151 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+
+
+namespace TestProject_1710
+
+{
+    [TestFixture]
+
+
+    public class HomePageTests
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl("https://localhost:7176/"); // Thay đổi URL nếu cần
+        }
+        [Test]
+        public void TestDN()
+        {
+            // Tìm nút Đăng nhập (DN)
+            IWebElement DnButton = _driver.FindElement(By.Name("DN"));
+            Assert.IsNotNull(DnButton);
+            DnButton.Click();
+
+            // Nhập thông tin số điện thoại
+            IWebElement phoneNameField = _driver.FindElement(By.Id("phoneNumber"));
+            phoneNameField.SendKeys("0466155193");
+
+            // Nhập mật khẩu
+            IWebElement passWordField = _driver.FindElement(By.Id("password"));
+            passWordField.SendKeys("0466155193");
+
+            // Nhấn nút Đăng nhập
+            IWebElement login = _driver.FindElement(By.Name("DangNhap"));
+            login.Click();
+
+            // Kiểm tra đã chuyển hướng sau khi đăng nhập thành công
+            Assert.AreEqual("https://localhost:7176/User/Index", _driver.Url);
+
+            // Chờ cho nút "Đóng" modal xuất hiện và sẵn sàng để click
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            IWebElement close = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("buttonCloseModal")));
+
+            // Cuộn tới phần tử nếu cần (nếu nó nằm ngoài màn hình)
+            ((IJavaScriptExecutor)_driver).ExecuteScript("arguments[0].scrollIntoView(true);", close);
+
+            // Nhấn vào nút "Đóng"
+            close.Click();
+        }
+
+        [Test]
+        public void TestDNEmptyUser()
+        {
+            IWebElement DnButton = _driver.FindElement(By.Name("DN"));
+            Assert.IsNotNull(DnButton);
+            DnButton.Click();
+
+            IWebElement phoneNameField = _driver.FindElement(By.Id("phoneNumber"));
+            phoneNameField.SendKeys("");
+            IWebElement passWordField = _driver.FindElement(By.Id("password"));
+            passWordField.SendKeys("0466155193");
+
+            IWebElement login = _driver.FindElement(By.Name("DangNhap"));
+            login.Click();
+
+            // Kiểm tra
+            Assert.AreEqual("https://localhost:7176/User/Index", _driver.Url);
+
+        }
+
+        [Test]
+        public void TestDNEmptyPassword()
+        {
+            IWebElement DnButton = _driver.FindElement(By.Name("DN"));
+            Assert.IsNotNull(DnButton);
+            DnButton.Click();
+
+            IWebElement phoneNameField = _driver.FindElement(By.Id("phoneNumber"));
+            phoneNameField.SendKeys("0466155193");
+            IWebElement passWordField = _driver.FindElement(By.Id("password"));
+            passWordField.SendKeys("");
+
+            IWebElement login = _driver.FindElement(By.Name("DangNhap"));
+            login.Click();
+
+            // Kiểm tra
+            Assert.AreEqual("https://localhost:7176/User/Index", _driver.Url);
+        }
+        [Test]
+        public void TestDNFailedPassword()
+        {
+            IWebElement DnButton = _driver.FindElement(By.Name("DN"));
+            Assert.IsNotNull(DnButton);
+            DnButton.Click();
+
+            IWebElement phoneNameField = _driver.FindElement(By.Id("phoneNumber"));
+            phoneNameField.SendKeys("0466155193");
+            IWebElement passWordField = _driver.FindElement(By.Id("password"));
+            passWordField.SendKeys("sfgxfsssszdd");
+
+            IWebElement login = _driver.FindElement(By.Name("DangNhap"));
+            login.Click();
+
+            // Kiểm tra
+            Assert.AreEqual("https://localhost:7176/User/Index", _driver.Url);
+        }
+        [Test]
+        public void TestDNFailedPhoneNumber()
+        {
+            IWebElement DnButton = _driver.FindElement(By.Name("DN"));
+            Assert.IsNotNull(DnButton);
+            DnButton.Click();
+
+            IWebElement phoneNameField = _driver.FindElement(By.Id("phoneNumber"));
+            phoneNameField.SendKeys("0886498002");
+            IWebElement passWordField = _driver.FindElement(By.Id("password"));
+            passWordField.SendKeys("0466155193");
+
+            IWebElement login = _driver.FindElement(By.Name("DangNhap"));
+            login.Click();
+
+            // Kiểm tra
+            Assert.AreEqual("https://localhost:7176/User/Index", _driver.Url);
+        }
+
+        [Test]
+        public void TestDK()
+        {
+            IWebElement DnButton = _driver.FindElement(By.Name("DN"));
+            Assert.IsNotNull(DnButton);
+            DnButton.Click();
+
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            // Đợi 5 giây
+            await Task.Delay(5000);
+
+            // Đóng trình duyệt
+            _driver.Dispose();
+        }
+    }
+}
--- WebAPI/Models/QuanLyThuVienContext.cs
+++ WebAPI/Models/QuanLyThuVienContext.cs
@@ -56,8 +56,8 @@ public partial class QuanLyThuVienContext : DbContext
     public virtual DbSet<TtSach> TtSaches { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=MSI;Initial Catalog=QuanLyThuVien_API;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=KT;Initial Catalog=QuanLyThuVien_API;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
